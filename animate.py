160a6fd8b698223334c958fea0a83d3f5c9628dc
pmorande27 <pmorande27@gmail.com>
2022-07-17 15:44:53 +0200

    Add energy graph animation

--- animate.py
+++ animate.py
@@ -12,7 +12,6 @@ class animate(object):
         self.history = np.array([[]])
         self.history = [self.model.lattice.copy()]
         
-        
 
        
     def evolve(self):
@@ -44,3 +43,32 @@ class animate(object):
         plt.title("Ising Model: ", fontsize=16)
         plt.imshow(self.model.lattice)
         plt.show()
+    def updatefig(self,i):
+        self.model.update()
+        self.im.set_array(self.model.lattice)
+        return [self.im]
+    def init(self):
+        self.im = plt.imshow(self.model.lattice)
+        return [self.im]
+    def animation(self):
+        
+        fig = plt.figure()
+        ani = animation.FuncAnimation(fig, self.updatefig,init_func=self.init,interval = 0.01, blit = True)
+        plt.show()
+    def update(self,num,x,y, line):
+        x += [num]
+        self.model.update()
+        y += [Model.get_energy(self.model.lattice)]
+        line.set_data(x, y)
+        line.axes.axis([0, self.iterations, -10000, 0])
+        return line,
+    def energy_animation(self):
+        fig, ax = plt.subplots()
+        x = []
+        y = []
+        line, = ax.plot([], [], color='k')
+        ani = animation.FuncAnimation(fig, self.update, frames =self.iterations,fargs=[x,y,line],
+                              interval=0.1, blit=True,repeat=False)
+        plt.show()
+
+        
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from model import Model
 from animate import animate
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import colors
+from matplotlib import patches
 def main():
-    a = animate(150,1,5000)
-    a.animate()
+    a = animate(50,20,10000)
+    a.energy_animation()
 
 main()
+'''
+model = Model(150,1)
+model.set_up_random_state()
+im = plt.imshow(model.lattice,animated=True)
+fig = plt.figure()
+
+def updatefig(i):
+    model.update()
+    im.set_array(model.lattice)
+    return [im]
+ani = animation.FuncAnimation(fig, updatefig,interval = 50, blit = True)
+plt.show()
+'''
