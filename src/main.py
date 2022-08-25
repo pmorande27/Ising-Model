1d83947f08790542642458dc72318cb893c121c1
Pablo <pmorande27@gmail.com>
2022-08-25 22:27:20 +0200

    Clean main

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ Main file
 """
 from animate import Animation
 from animate_variable_temperature import Animation_variable_temperature
+from model import Model
+import matplotlib.pyplot as plt
 def main():
     """
     Main method
     """
-    animation = Animation_variable_temperature(50, 10,2, 0.001, 10000*2,0)
+    animation = Animation_variable_temperature(100, 10,2, 0.001, 10000*4,0)
     animation.all_animations()
-
 main()
