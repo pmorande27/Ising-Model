fc3ff77d5d2f1eaab4e03c5f09129e9178781e5c
Pablo <pmorande27@gmail.com>
2022-08-25 22:37:07 +0200

    Add final animation and delete redundant animations

--- src/animate.py
+++ src/animate.py
@@ -17,6 +17,7 @@ class Animation(object):
         self.iterations = iterations
         self.history = np.array([[]])
         self.history = [self.model.lattice.copy()]
+        self.H = H
         
     def evolve(self):
         """
@@ -88,77 +89,38 @@ class Animation(object):
         ani = animation.FuncAnimation(fig, self.updatefig,init_func=self.init, blit = True)
         plt.show()
         
-    def update(self,num,x,y, line):
-        """
-        Method used to update the model to get the energy for animation
-        """
-        x += [num]
+    def update_all(self,num, x_energy, y_energy, x_magnet, y_magnet, line_energy,line_magnet):
+        x_energy += [num]
+        x_magnet += [num]
         self.model.update()
-        y += [Model.get_energy(self.model.lattice)]
-        line.set_data(x, y)
-        line.axes.axis([0, self.iterations, -4*self.model.N*self.model.N, 0])
-        return line,
+        y_energy += [Model.get_energy(self.model.lattice,self.H)]
+        y_magnet += [self.model.get_magnetisation()]
+        line_energy.set_data(x_energy, y_energy)
+        line_magnet.set_data(x_magnet, y_magnet)
+        self.im.set_array(self.model.lattice)
+        self.temp.set_height(self.model.T)
+        line_magnet.axes.axis([0, self.iterations, -self.model.N**2, self.model.N**2])
+        line_energy.axes.axis([0, self.iterations, -4*self.model.N*self.model.N, 0])
 
-    def energy_animation_save(self):
-        """
-        Method used to create an animation of the energy of the system. It saves the animation
-        """
-        fig, ax = plt.subplots()
-        x = []
-        y = []
-        line, = ax.plot([], [], color='k')
-        ani = animation.FuncAnimation(fig, self.update, frames =self.iterations,fargs=[x,y,line],
-                              interval=0.1, blit=True,repeat=False)
-        ani.save('animation.gif', writer='imagemagick', fps=120)
+        return [self.im, self.temp, line_energy,line_magnet]
 
-    def energy_animation(self):
-        """
-        Method used to create an animation of the energy of the system. It saves the animation
-        """
-        fig, ax = plt.subplots()
-        x = []
-        y = []
-        line, = ax.plot([], [], color='k')
-        ani = animation.FuncAnimation(fig, self.update, frames =self.iterations,fargs=[x,y,line],
+        
+    def all_animations(self):
+        fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2)
+        x_energy = []
+        y_energy = []
+        line_energy, = ax3.plot([],[],color ='k')
+        x_magnet = []
+        y_magnet = []
+        line_magnet, = ax4.plot([],[],color='k')
+        self.im = ax1.imshow(self.model.lattice)
+        self.temp, = ax2.bar(' ',self.model.T)
+        ax2.set_title('Temperature')
+        ax1.set_title('Ising Model')
+        ax3.set_title('Energy')
+        ax4.set_title('Magnetization')
+        ani = animation.FuncAnimation(fig, self.update_all, frames =self.iterations,fargs=[x_energy,y_energy,x_magnet,y_magnet,line_energy,line_magnet],
                               interval=0.1, blit=True,repeat=False)
         plt.show()
 
-    def get_energy(self):
-        """
-        Method used to update the system a set number of iterations to get the final energy of the system.
-        """
-        for i in range(self.iterations):
-            self.model.update()
-            print(Model.get_energy(self.model.lattice,self.model.H))
-
-    def get_lattice(self):
-        """
-        Method used to update the system a set number of iterations to get the final matrix
-        """
-        for i in range(self.iterations):
-            self.model.update() 
-        return self.model.lattice
-    
-    def update_magnet(self,num,x,y, line):
-        """
-        Method used to update the model to get the energy for animation
-        """
-        x += [num]
-        self.model.update()
-        y += [self.model.get_magnetisation()]
-        line.set_data(x, y)
-        line.axes.axis([0, self.iterations, -self.model.N**2, self.model.N**2])
-        return line,
-    
-    def magnet_animation(self):
-        """
-        Method used to create an animation of the energy of the system. It saves the animation
-        """
-        fig, ax = plt.subplots()
-        x = []
-        y = []
-        line, = ax.plot([], [], color='k')
-        ani = animation.FuncAnimation(fig, self.update_magnet, frames =self.iterations,fargs=[x,y,line],
-                              interval=0.1, blit=True,repeat=False)
-        plt.show()
     
